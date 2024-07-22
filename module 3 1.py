8e2e4b4b59fae351282ce6f29ab1c62660ae3a9e
Anna-Andr-new <tsukanova0910anna@gmail.com>
2024-07-22 09:46:56 +0300

    Add files via upload

---
+++ module 3 1.py
@@ -0,0 +1,14 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains (string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+print(string_info ('Good'))
+print(string_info ('Cow'))
